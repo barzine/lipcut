3584a9ecc773f6eac87960482f420739f58dcd3a
M.P. Barzine <barzine@gmail.com>
2014-04-29 16:48:58 +0100

    Update utils.py
    some translation - and splitting the utils in two files:
    one python wide, the other pymol centric

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ from pymol import cmd
 #def affichage_boucle(Liste):
 def print_loop(Liste):
     """
-    Procedure printing the content of one liste, line per line
+   Print the content of a liste, line per line
     """ 
     for elmt in Liste :
         print elmt 
@@ -45,7 +45,7 @@ def print_loop(Liste):
 #def affichage_boucle_liste_dans_list(Liste):
 def print_loop_in_loop(Liste):
     """
-    Procedure printing line per line the content of lists themselves included in a list 
+    Print line per line the content of nested lists (inside a list) 
     """ 
     il=0 #initialisation of the counter for the sub-lists
     for sublist in Liste : #for each sublist in the main list
@@ -61,162 +61,34 @@ def print_loop_in_loop(Liste):
     print "\nThe main list contains "+il+" sublist(s)."
 ##
 
-def affichage_dico(dico):
+#def affichage_dico(dico):
+def dico_print(dico):
     """
-        permet d'afficher le contenu d'un dico de maniere un peu plus intelligible
+        print the content of a dictionary in a more human-lisible way
     """
     
-    cles=dico.keys()
-    print "cle, valeur"
-    for c in cles:
-        print c,dico[c] 
+    ring=dico.keys()
+    print "key, valeur"
+    for k in ring:
+        print k,dico[k] 
 
 
 
-#Fonctions de recopie 
-#pour palier au pointage dynamique rÈalisÈ par Python au lieu des affectations souhaitÈes
-def copy(liste_source):
+#Copy function ... before I learned about copy and the magic deep.copy function 
+#overide the dynamic allocation by a static one
+def copy(sourceList,verbose=False):
     """
-        Fonction dÈfinie car python ne copie pas les donnÈes,
-        mais crÈe, ‡ la place, un synonyme pour la liste
+        created before being aware of the deep.copy [copy] function
+        return a new object instead of a synonym
     """
-    #print "dans copy" #debug : affichage de contrÙle
-    liste_a_retourner=[] #initialisation de la structure mÈmorisant la liste ‡ retourner
-    #print "initialisation effectue" #debug : affichage de contrÙle
-    for i in liste_source: #pour chaque element i de la liste_source
-    #   print i #debug : affichage de contrÙle
-        liste_a_retourner.append(i) #ajout de l'ÈlÈment
-    #print "on sort de copy" #debug : affichage de contrÙle
-    return(liste_a_retourner) #permet de crÈer une liste identique a celle reÁue en paramËtre
+   
+    newList=[] #initialisation
 
+    for i in sourceList: #for each i element of sourceList
+        if (verbose):
+            print i #for debugging purpose
+        newList.append(i) #ajout de l'ÈlÈment
 
-#fonctions travaillant avec PyMOL :
+    return(newList) #the new list is returned to the main environment, hence creating the new object
 
-def removeAlt(obj="(all)", keep="A"):
-        """
-        from PyMOLWIKI
-        removeAlt -- remove all alternate location-atoms not of altloc "keep" from object.
- 
-        input:
-                obj -- the object(s) to remove the atoms frmo
-                keep -- which type of alt loc to keep
- 
-        output: none -- removes atoms
- 
-        examples:
-                removeAlt # remove all altLocations that aren't altloc A
-                removeAlt pdbID, C  # remove all but C altlocations from pdbID
-        """
-        #select & remove all non A altlocs
-        remStr = "%s and not (alt ''+%s)" % (obj, keep);
-        cmd.remove(remStr);
-        #reset the PDB information
-        #cmd.alter( obj, "alt=''")
-
-def chargement_fichier_pdb(filename):
-    """
-        permet de charger le fichier pdb et de faire les pretraitements necessaires au bon fonctionnement du script dans pyMOL
-    """
-    cmd.delete("all") #initialise l'espace de travail dans pymol
-    cmd.load(filename) #charge le fichier spÈcifiÈ par filename
-    removeAlt()#retire tous jeux de coordonnÈes alternatif pour un atome donnÈ
-    cmd.remove("hetatm")#permet de retirer les heteroatoms #attention : parfois certains fichiers sont mal annotÈs : un carbone alpha peu Ítre dÈcrit comme Ètant un hÈtÈro-atome
-    cmd.remove("h.")#permet de retirer toutes les molÈcules d'eau
-    cmd.hide("all")#permet de cacher l'ensemble de la structure
-    #cmd.show("ribbon")#rÈaffiche la structure sous le format ruban
-    cmd.show("cartoon")#rÈaffiche la structure sous le format cartoon
-    cmd.color("gray30")#colorise l'ensemble de gris - pour faire ressortir ultÈrieurement par contraste les fragments sÈlectionnÈs
-
-
-def chargement_fichier_pdb2(filename):
-    """
-        permet de charger le fichier pdb et de faire les pretraitements necessaires au bon fonctionnement du script dans pyMOL
-    """
-    #cmd.delete("all") #initialise l'espace de travail dans pymol
-    cmd.load(filename) #charge le fichier spÈcifiÈ par filename
-    removeAlt()#retire tous jeux de coordonnÈes alternatif pour un atome donnÈ
-    cmd.remove("hetatm")#permet de retirer les heteroatoms #attention : parfois certains fichiers sont mal annotÈs : un carbone alpha peu Ítre dÈcrit comme Ètant un hÈtÈro-atome
-    cmd.remove("h.")#permet de retirer toutes les molÈcules d'eau
-    cmd.hide("all")#permet de cacher l'ensemble de la structure
-    #cmd.show("ribbon")#rÈaffiche la structure sous le format ruban
-    cmd.show("cartoon")#rÈaffiche la structure sous le format cartoon
-    #cmd.color("gray30")#colorise l'ensemble de gris - pour faire ressortir ultÈrieurement par contraste les fragments sÈlectionnÈs
-
-
-def identificateur_pymol(atom):
-    """
-        a partir d'un tuple permettant de decrire un atome,
-        renvoie une chaine de caractere permettant de manipuler cet atome dans pymol
-    """
-    #champ[1] de atom contient l'identificateur de chaine du residu
-    #champ[0] de atom contient le numÈro du rÈsidu, 
-    
-    return ("(c;"+atom[1]+"&i;"+atom[0]+"&n;ca)")
-
-##PyMOL et visualisation 
-def color_struct(elmt,t_elmt):
-    """
-        permet de colorer la structure specifier selon sa nature, permet ainsi de visualiser facilement les elements selectionnes
-        pour un controle visuel de l'utilisateur
-    """
-    
-    #"c;A&i;87&n;ca"
-    #cmd.show("spheres","c;A&i;87&n;ca")
-    #cmd.color("orange","c;A&i;87&n;ca")
-    #cmd.color("red","c;A&i;83&n;ca")
-    
-    #print "dans la fonction color_struct_beta_ser"#debug
-    #print elmt,t_elmt#debug
-    
-    if t_elmt=="SER":
-        cmd.show("spheres",identificateur_pymol(elmt))
-        #cmd.color("red",identificateur_pymol(elmt))
-    
-    elif t_elmt=="SER_CAT":
-        cmd.color("chocolate",identificateur_pymol(elmt))
-            
-    elif t_elmt=="B5":
-        for e in elmt:
-            cmd.color("red",identificateur_pymol(e))
-    
-    elif t_elmt=="B6":
-        for e in elmt:
-            cmd.color("orange",identificateur_pymol(e))
-    
-    elif t_elmt=="B7":
-        for e in elmt:
-            cmd.color("tv_orange",identificateur_pymol(e))
-    
-    elif t_elmt=="B8":
-        for e in elmt:
-            cmd.color("brightorange",identificateur_pymol(e))
-            
-    elif t_elmt=="B4":
-        for e in elmt:
-            cmd.color("hotpink",identificateur_pymol(e))
-    
-    elif t_elmt=="B3":
-        for e in elmt:
-            cmd.color("magenta",identificateur_pymol(e))
-    
-    elif t_elmt=="B2":
-        for e in elmt:
-            cmd.color("lightpink",identificateur_pymol(e))
-    
-    #elif t_elmt=="B_AUTRE":
-    #    for e in elmt:
-    #        cmd.color("blue",identificateur_pymol(e))
-    
-    elif t_elmt=="B_AUTRE2":
-        for e in elmt:
-            cmd.color("skyblue",identificateur_pymol(e))
-    #print "on sort de la fonction color_struct_beta_ser"#debug
-    
-    elif t_elmt=="ACIDE":
-        cmd.show("spheres",identificateur_pymol(elmt))
-        cmd.color("wheat",identificateur_pymol(elmt))
-    
-    elif t_elmt=="HIS":
-        cmd.show("spheres",identificateur_pymol(elmt))
-        cmd.color("yellow",identificateur_pymol(elmt))
 
